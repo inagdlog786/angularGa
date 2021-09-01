591e9cc004109c321bf82e7348fa208c5540e780
Venkata Ganesh <inagdlog786@gmail.com>
2021-09-01 20:09:12 +0530

    Multi Dropdown selection

--- src/app/allc/tooltip/tooltip.component.ts
+++ src/app/allc/tooltip/tooltip.component.ts
@@ -9,10 +9,64 @@ export class TooltipComponent implements OnInit {
 
   // tooltipInnerHtml = ``;
 
+
+  isShowMultiSelect = true;
+   
+  openMultiselect() {
+    this.isShowMultiSelect = !this.isShowMultiSelect;
+  }
+
+  //
+  viewAll=true;
+  data=[
+    {label:'BikebikebikebikebikebikebikeBikebikebikebikebikebikebike',value:'bikebikebikebikebikebikebike',selected:true},
+    {label:'Car',value:'Car',selected:false},
+    {label:'Cycle',value:'Cycle',selected:false},
+    {label:'Bike1',value:'Bike1',selected:false},
+    {label:'Car1',value:'Car1',selected:false},
+    {label:'Cycle1',value:'Cycle1',selected:false},
+    {label:'Bike2',value:'Bike2',selected:false},
+    {label:'Car2',value:'Car2',selected:false},
+    {label:'Cycle2',value:'Cycle2',selected:false},
+    {label:'Bike3',value:'Bike3',selected:false},
+    {label:'Car3',value:'Car3',selected:false},
+    {label:'Cycle3',value:'Cycle3',selected:false},
+    {label:'Bike4',value:'Bike4',selected:false},
+    {label:'Car4',value:'Car4',selected:false},
+    {label:'Cycle4',value:'Cycle4',selected:false}
+  ];
+
+  selecteddata:any[]=[];  
+  selectall:boolean=false;
+
+  optionsModel: number[] = [];
+  //
+
   constructor() { }
 
   ngOnInit(): void {
+    this.onFindSelectedList();
   }
+
+  onSelect(item:any){
+    this.data.filter(selectCheckbox=>{
+        if(selectCheckbox.label===item.label){
+          selectCheckbox.selected=false;
+        }      
+      });
+      this.onFindSelectedList();
+    }
+
+    onFindSelectedList(){
+      this.selecteddata=[];
+      this.data.forEach(checklist=>{
+        if(checklist.selected){
+          this.selecteddata.push(checklist);
+        }
+      });
+      this.selectall= (this.data.length === this.selecteddata.length) ? true:false;
+    }
+  
 
   
 
